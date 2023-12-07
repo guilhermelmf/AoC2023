cb4288bd28a04c5f5c11b2491d5912cfea91cb9c
Guilherme Lopes de Morais Filho <guilherme.lopesdemoraisfilho@thetrainline.com>
2023-12-07 17:58:09 +0000

    Day 2

---
+++ AoC2023/Day2/Day2.cs
@@ -0,0 +1,65 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AoC2023.Day2;
+
+public class Day2
+{
+    //Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    private record Game(int Match, int Reds, int Blues, int Greens);
+
+    private static readonly Regex GameNumber = new Regex("(?<=Game\\s)\\d{1,5}");
+    private static readonly Regex GameBlue = new Regex("\\d{1,5}(?=\\sblue)");
+    private static readonly Regex GameRed = new Regex("\\d{1,5}(?=\\sred)");
+    private static readonly Regex GameGreen = new Regex("\\d{1,5}(?=\\sgreen)");
+
+    public int ResolvePart1(List<string> lines, int maxReds, int maxBlues, int maxGreens)
+    {
+        var games = lines.Where(x=> !string.IsNullOrEmpty(x)).Select(ParseGame).ToList();
+
+        return games
+            .Where(x => x.Blues <= maxBlues &&
+                        x.Reds <= maxReds &&
+                        x.Greens <= maxGreens)
+            .Sum(x=> x.Match);
+    }
+    public int ResolvePart2(List<string> lines)
+    {
+        var games = lines.Where(x=> !string.IsNullOrEmpty(x)).Select(ParseGame).ToList();
+
+        return games.Sum(x=> x.Reds * x.Blues * x.Greens);
+    }
+
+    private Game ParseGame(string line)
+    {
+        var match = int.Parse(GameNumber.Match(line).Value);
+        var sets = line.Split(":").Last().Split(";");
+        var redMax = 0;
+        var greenMax = 0;
+        var blueMax = 0;
+        foreach (var set in sets)
+        {
+            var value =$"0{GameRed.Match(set).Value}";
+            var reds = int.Parse(value);
+            if (reds > redMax)
+            {
+                redMax = reds;
+            }
+
+            value =$"0{GameBlue.Match(set).Value}";
+            var blues = int.Parse(value);
+            if (blues > blueMax)
+            {
+                blueMax = blues;
+            }
+
+            value =$"0{GameGreen.Match(set).Value}";
+            var greens = int.Parse(value);
+            if (greens > greenMax)
+            {
+                greenMax = greens;
+            }
+        }
+
+        return new Game(match, redMax, blueMax, greenMax);
+    }
+}
--- AoC2023/Program.cs
+++ AoC2023/Program.cs
@@ -2,15 +2,21 @@
 
 
 using AoC2023.Day1;
+using AoC2023.Day2;
 using AoC2023.Helpers;
 
 var sessionId = "53616c7465645f5ff9bbf90355d83d2afb839bfaff8059a3e85fabd70f26af0260fa841df8f6948133ca14aa6e6385000b7f8be2df0c2a24ab62a5f38cb99a2a";
 
 var dataDay1 = await InputHelper.GetDataLines(1, sessionId);
+var dataDay2 = await InputHelper.GetDataLines(2, sessionId);
 
 var day1 = new Day1();
 
 Console.WriteLine($"Day 1 - part 1: result {day1.ResolvePart1(dataDay1)}");
 Console.WriteLine($"Day 1 - part 2: result {day1.ResolvePart2(dataDay1)}");
+
+var day2 = new Day2();
+Console.WriteLine($"Day 2 - part 1: result {day2.ResolvePart1(dataDay2, 12, 14, 13)}");
+Console.WriteLine($"Day 2 - part 2: result {day2.ResolvePart2(dataDay2)}");
 
 
